738535eea39f8ad58af718b77ce1ac8ef70fb85d
Erich Schulz <erichbschulz@gmail.com>
2016-12-24 07:54:43 +0000

    attempted hello world

---
+++ api/v3/Contact.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Contact.Get API specification
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
+ */
+function _civicrm_api3_contact_getclif_spec(&$spec) {
+  $spec['clif']['api.required'] = 1;
+}
+
+/**
+ * Contact.Get API
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_contact_getclif($params) {
+  $defaults = array(
+    'clif' => false,
+    'return' => 'id',
+  );
+  // merge params in with defaults
+  $p = $params + $defaults;
+  // validate parameters
+  if (!$p['clif']) {
+    throw new API_Exception('Missing "clif" parameter');
+  }
+  if ($p['return'] != 'id') {
+    throw new API_Exception('Can only return ID until Erich is pointed in right
+      direction');
+  }
+  $contacts = AgcCliff::get($p);
+  return civicrm_api3_create_success(
+    $contacts, //
+    $params, // todo ?? clarify best as $p or $params
+    'Contact',
+    'Get'); // todo get or Get??
+}
+
+  $params['return'] = 'id';
+
+  return civicrm_api3('Contact', 'get', $params);
+
+  class AgcCliff {
+
+    public static function get($params) {
+      return 'hello world';
+    }
+
+  }
